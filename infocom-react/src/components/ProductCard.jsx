08405c9c858bac2080879e543c3e85beec51d722
kauyves <motakauyves@gmail.com>
2025-12-16 19:18:44 -0300

    :sparkles: atualização do código com mais detalhes dos produtos

--- infocom-react/src/components/ProductCard.jsx
+++ infocom-react/src/components/ProductCard.jsx
@@ -1,13 +1,21 @@
 import './ProductCard.css';
+import { useNavigate } from "react-router-dom";
 
 function ProductCard({ product }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/products/${product.id}`);
+  };
   return (
-    <div className="card">
+    <div className='card'>
       <img src={product.image} alt={product.title} />
+
+      <div className='separator'></div>
+
+      <div className='price'>R$ {product.price.toFixed(2).replace(".", ",")}</div>
+
       <h3>{product.title}</h3>
-      <div className="price">
-        R$ {product.price.toFixed(2).replace('.', ',')}
-      </div>
     </div>
   );
 }
